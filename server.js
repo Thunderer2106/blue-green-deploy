fc5770a009ce89fd07e2d7e237e46022810825e7
Thunderer2106 <114282536+Thunderer2106@users.noreply.github.com>
2024-11-08 10:40:19 +0530

    Initial commi

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Hello from Blue-Green Deployment!"));
+app.get("/", (req, res) =>
+  res.send("Hello from Blue-Green Deployment is here!")
+);
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
